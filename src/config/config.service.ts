2716f764675848f4a426ec1f4abe925744968221
Urs97 <64044473+Urs97@users.noreply.github.com>
2025-03-29 19:39:06 +0100

    feat(config): setup global config service for token secrets

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -26,4 +26,24 @@ export class AppConfigService {
   get dbName(): string {
     return dbConfig.database;
   }
+
+  get frontendOrigin(): string {
+    return process.env.FRONTEND_ORIGIN!;
+  }
+
+  get jwtSecret(): string {
+    return process.env.JWT_SECRET!;
+  }
+
+  get refreshTokenSecret(): string {
+    return process.env.REFRESH_TOKEN_SECRET!;
+  }
+
+  get accessTokenExpiresIn(): string {
+    return process.env.ACCESS_TOKEN_EXPIRES_IN!;
+  }
+
+  get refreshTokenExpiresIn(): string {
+    return process.env.REFRESH_TOKEN_EXPIRES_IN!;
+  }
 }
