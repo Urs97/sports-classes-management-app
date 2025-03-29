85ee4d12f74ca0b7ce581ff792de822d6a33d4c6
Urs97 <64044473+Urs97@users.noreply.github.com>
2025-03-29 21:35:41 +0100

    chore(config): enforce required DB_HOST and DB_PORT without defaults

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -9,8 +9,8 @@ import { TypeOrmConfigService } from './database/typeorm-config.service';
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        DB_HOST: Joi.string().default('localhost'),
-        DB_PORT: Joi.number().default(5432),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
         DB_USER: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
