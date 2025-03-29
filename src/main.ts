0e5f507eefc9561c4b0fe868d6220b1ecb3af554
Urs97 <64044473+Urs97@users.noreply.github.com>
2025-03-29 19:40:09 +0100

    feat(main): enable CORS for security & add cookieParser

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { AppConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(AppConfigService);
+
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
+
+  app.enableCors({
+    origin: config.frontendOrigin,
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
