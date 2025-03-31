695d6455bc62e1ff5e7070e92a5aed0382fcf3fc
Urs97 <64044473+Urs97@users.noreply.github.com>
2025-03-31 05:31:47 +0200

    chore: remove redundant dotenv.config() call from AppDataSource

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,10 @@
-import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
+import { dbConfig } from './config/database/db-config';
 import { User } from './users/entities/user.entity';
 import { Sport } from './sports/entities/sport.entity';
 import { Class } from './classes/entities/class.entity';
 import { Schedule } from './schedules/entities/schedule.entity';
 import { Enrollment } from './enrollments/entities/enrollment.entity';
-import { dbConfig } from './config/database/db-config';
-
-dotenv.config();
 
 export const AppDataSource = new DataSource({
   ...dbConfig,
