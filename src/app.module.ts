7d582c5a7287b732087de6e88913e0b184989d03
Urs97 <64044473+Urs97@users.noreply.github.com>
2025-03-31 03:40:51 +0200

    chore: remove unused app controller, service, and test files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './config/database/typeorm-config.service';
 import { AppConfigModule } from './config/config.module';
@@ -34,7 +32,7 @@ import { ThrottlerModule } from '@nestjs/throttler';
     SchedulesModule,
     EnrollmentsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
